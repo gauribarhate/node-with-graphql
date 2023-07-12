158df68fcc40f29a20038f1f33d87bd55e3ad034
Barhate <Gunavanti.Barhate@emtecinc.com>
2023-07-12 17:03:28 +0530

    Added broker port

--- kafka-consumer/consumer.js
+++ kafka-consumer/consumer.js
@@ -1,15 +1,14 @@
-const { Kafka } = require("kafkajs");
+const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
-  clientId: "my-app",
-  brokers: ["localhost:9092"],
+  brokers: ['localhost:29092'],
 });
 
 const createConsumer = async () => {
-  const consumer = kafka.consumer({ groupId: "test-group" });
+  const consumer = kafka.consumer({ groupId: 'test-group' });
 
   await consumer.connect();
-  await consumer.subscribe({ topic: "product-topic", fromBeginning: true });
+  await consumer.subscribe({ topic: 'product-topic', fromBeginning: true });
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
